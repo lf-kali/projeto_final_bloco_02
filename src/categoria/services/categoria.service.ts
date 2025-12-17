aef580e66f909f341688386f52bd0d4e1ccfc793
lf-kali <cbjk.kali@gmail.com>
2025-12-17 11:39:30 -0300

    feat: CRUD da categoria

---
+++ src/categoria/services/categoria.service.ts
@@ -0,0 +1,55 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ILike, Repository } from 'typeorm';
+import { Categoria } from '../entities/categoria.entity';
+import { DeleteResult } from 'typeorm/browser';
+
+@Injectable()
+export class CategoriaService {
+  constructor(private categoriaRepository: Repository<Categoria>) {}
+
+	async findByID(id: number): Promise <Categoria>{
+		const buscaCategoria = await this.categoriaRepository.findOne({
+			where: {
+				id,
+			},
+		});
+		if(!buscaCategoria) {
+			throw new HttpException(`Categoria de id ${id} não encontrada!`, HttpStatus.NOT_FOUND);
+		}
+
+		return buscaCategoria;
+	}
+	
+	async findAll(): Promise<Categoria[]> {
+		return await this.categoriaRepository.find();
+	}
+
+	async findAllByDescricao(descricao: string): Promise<Categoria[]> {
+		return await this.categoriaRepository.find({
+			where: {
+				descricao: ILike(`%${descricao}%`)
+			}
+		})
+	}
+
+	async create(categoria: Categoria): Promise<Categoria> {
+		return await this.categoriaRepository.save(categoria);
+	}
+
+	async update(categoria: Categoria): Promise<Categoria> {
+		if(!categoria.id) {
+			throw new HttpException("ID da categoria é obrigatório para atualizar!", HttpStatus.BAD_REQUEST);
+		}
+
+		await this.findByID(categoria.id);
+
+		return await this.categoriaRepository.save(categoria);
+	}
+
+	async delete(id: number): Promise<DeleteResult> {
+		await this.findByID(id);
+
+		return await this.categoriaRepository.delete(id);
+	}
+
+}
